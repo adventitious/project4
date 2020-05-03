65f4f1d206331849073fa32e857e37062b29bb4e
s <s@s.com>
2020-05-03 11:44:02 +0100

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IngredientComponent } from './ingredient/ingredient.component';
+import { PressComponent } from './press/press.component';
+import { SandwichComponent } from './sandwich/sandwich.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IngredientComponent
+    IngredientComponent,
+    PressComponent,
+    SandwichComponent
   ],
   imports: [
     BrowserModule
